efae1caf06be8eb6fd43559205caeb8f941b03e2
severogianluca <gianlucasevero300@gmail.com>
2025-03-06 18:57:34 +0100

    Feat: create random number

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,6 +1,7 @@
 let elementCountdown = document.getElementById ('countdown');
 let elementAnswers = document.getElementById('answers-form');
 let elementInstructions = document.getElementById('instructions')
+let elementNumber = document.getElementById('numbers-list');
 
 let count = 10;
 let countDown = setInterval(function () {
@@ -8,10 +9,25 @@ let countDown = setInterval(function () {
     count--;
     
     if (count === -1) {
-        elementAnswers.classList.remove('d-none')
+        elementNumber.classList.add('d-none');
+        elementAnswers.classList.remove('d-none');
         elementInstructions.innerText = `Inserisci i numeri che ricordi (l'ordine non è importante!)`
         clearInterval(countDown);
     }
 }, 1000);
 
 
+
+function randomNumber (){
+    return Math.ceil(Math.random() * 50);
+}
+
+for(let i = 0; i < 5; i++){
+    let number = randomNumber();
+    console.log(number);
+    
+    let listItem = document.createElement('li'); // Crea un <li>
+    listItem.textContent = number; // Imposta il testo del <li>
+    elementNumber.appendChild(listItem); // Aggiunge il <li> alla <ul>
+    
+}
